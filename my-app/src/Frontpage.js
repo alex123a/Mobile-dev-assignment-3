e4af5ff4b91c91f5b219666903ccdf1bcf55181b
Alex Valentiner <45672183+alex123a@users.noreply.github.com>
2022-11-16 21:52:43 +0100

    IDs

--- my-app/src/Frontpage.js
+++ my-app/src/Frontpage.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 import './App.css';
+import { v4 as uuid } from "uuid";
 
 function App() {
     const inputRef = useRef(null);
@@ -11,27 +12,37 @@ function App() {
             a new TODO. So it removes the text before actually adding the input so the value will be 0 when it is added. To fix this I just
             save the value in a new variable called value.
         */
-        const value = inputRef.current.value;
-        setValues( arr => [...arr, value]);
+        const temp = inputRef.current.value;
+        const value = {
+            id: uuid(),
+            content: temp
+        }
+        setValues( arr => [...arr, JSON.stringify(value)]);
         inputRef.current.value = "";
     };
 
     const handleRemoveItem = e => {
+        e = JSON.stringify(e);
         setValues(inputValues.filter(item => item !== e));
     };
     
     return (
-        <div>
+        <div id="frontPageDiv">
+            <p className="normalText">Add TODOs below:</p>
             <input id="TODOText" ref={inputRef} type="text"></input>
             <input id="addBut" type="button" onClick={ onClick } value="Add" />
-
-            <div>
-                {inputValues.map( e =>
-                    <div className="TODOs" name={e}>
-                        <span>{e}</span>
-                        <input className="TODOButton" type="button" value="Delete" name={e} onClick={() => handleRemoveItem(e)}></input>
-                    </div>
-                )}
+            
+            <div id="TODOsDiv">
+                <p className="normalText">TODO list:</p>
+                {inputValues.map((e) => {
+                    e = JSON.parse(e);
+                    return (
+                        <div key={e.id} className="TODOs" name={e}>
+                            <span className='TODOText'>{e.content}</span>
+                            <input className="TODOButton" type="button" value="Delete" name={e} onClick={() => handleRemoveItem(e)}></input>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     );
